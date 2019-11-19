c15f2dab668bc5b0810f09905cf6fb0684c211d6
bitflower <matthias.max@bitflower.net>
2019-11-19 18:21:24 +0100

    more changes

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -8,7 +8,22 @@ export class AppHome {
   render() {
     return (
       <Host>
-        <section style={{ 'background-color': 'var(--mm-primary)' }}>
+        <section
+          class='vh100'
+          style={{ 'background-color': 'var(--mm-primary)' }}
+        >
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
+          <h3>Heading 3</h3>
+          <mm-badge value='Hi' />
+          <mm-badge value='Hi' />
+          <mm-badge value='Hi' />
+          <mm-badge value='Hi' />
+        </section>
+        <section
+          class='vh50'
+          style={{ 'background-color': 'var(--mm-secondary)' }}
+        >
           <mm-badge value='Hi' />
         </section>
       </Host>
