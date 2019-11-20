ef782af00e472a625d8a9a9363cbffc2191b1a95
bitflower <matthias.max@bitflower.net>
2019-11-20 16:57:46 +0100

    progress

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -20,25 +20,28 @@ export class AppHome {
               </div>
             </div>
           </section>
-          <section class='vh50 bg-light-base matt color-dark-base'>
+          <section class='vh85 bg-light-base matt color-dark-base'>
             <div class='content text-center'>
               <h2>Hi, I'm Matt. Great to meet you.</h2>
               <p>
-                Since beginning my journey as a freelance designer nearly 8
-                years ago, I've done remote work for agencies, consulted for
-                startups, and collaborated with talented people to create
-                digital products for both business and consumer use. I'm quietly
-                confident, naturally curious, and perpetually working on
-                improving my chops one design problem at a time.
+                For the last 5 years I’ve been building enterprise application
+                for large corporations. Happily switching between requirements
+                engineering, UI and UX work, architecture. I like working with
+                talented people because I strongly believe that great value
+                comes from interdisciplinary collaboration.
               </p>
-              {/* <p>
-                Since beginning my journey as a freelance designer nearly 8
-                years ago, I've done remote work for agencies, consulted for
-                startups, and collaborated with talented people to create
-                digital products for both business and consumer use. I'm quietly
-                confident, naturally curious, and perpetually working on
-                improving my chops one design problem at a time.
-              </p> */}
+              <p>
+                In the past I've been a professional songwriter and music label
+                founder with international platin and gold records on my track
+                record.
+              </p>
+              <p>
+                Value on a larger scale is almost always the sum of small added
+                values. So always remember: LEGO blocks can also be disassembled
+                - which sometimes id the right thing to do ;-) Stick to
+                real-world values will almost always lead to the right decisions
+                - both technical and economical.
+              </p>
             </div>
           </section>
           <section class='vh50 bg-primary-base'>
