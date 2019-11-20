5462a610f0f44bdfa0a3dcf1bd39cc7e54c339e6
bitflower <matthias.max@bitflower.net>
2019-11-20 16:27:50 +0100

    more ...

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -20,15 +20,39 @@ export class AppHome {
               </div>
             </div>
           </section>
-          <section class='vh50 bg-light-base'>
+          <section class='vh50 bg-light-base matt color-dark-base'>
             <div class='content text-center'>
-              <h2>Hi, I'm Matt.</h2>
+              <h2>Hi, I'm Matt. Great to meet you.</h2>
+              <p>
+                Since beginning my journey as a freelance designer nearly 8
+                years ago, I've done remote work for agencies, consulted for
+                startups, and collaborated with talented people to create
+                digital products for both business and consumer use. I'm quietly
+                confident, naturally curious, and perpetually working on
+                improving my chops one design problem at a time.
+              </p>
+              {/* <p>
+                Since beginning my journey as a freelance designer nearly 8
+                years ago, I've done remote work for agencies, consulted for
+                startups, and collaborated with talented people to create
+                digital products for both business and consumer use. I'm quietly
+                confident, naturally curious, and perpetually working on
+                improving my chops one design problem at a time.
+              </p> */}
             </div>
           </section>
-          <section class='vh50 bg-light-base'>
-            <div class='content'>
-              <div class='bg-light-base overlap box overlap-container'>
-                <h2>Hi, I'm Matt.</h2>
+          <section class='vh50 bg-primary-base'>
+            <div class='content wide overlap'>
+              <div class='bg-light-base box overlap-container'>
+                <h2>More ...</h2>
+                <p>
+                  Since beginning my journey as a freelance designer nearly 8
+                  years ago, I've done remote work for agencies, consulted for
+                  startups, and collaborated with talented people to create
+                  digital products for both business and consumer use. I'm
+                  quietly confident, naturally curious, and perpetually working
+                  on improving my chops one design problem at a time.
+                </p>
               </div>
             </div>
           </section>
